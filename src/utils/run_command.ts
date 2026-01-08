d25807b1f183e6c63b020354711559e16b27380d
Anders Windelhed <anders.windelhed@gmail.com>
2026-01-08 21:56:23 +0100

    Running commands the shell definde in SHELL

--- src/utils/run_command.ts
+++ src/utils/run_command.ts
@@ -2,7 +2,7 @@ import { execa } from 'execa'
 
 const runCommand = async (command: string) => {
   try {
-    await execa(command, { shell: true })
+    await execa(command, { shell: process.env.SHELL || true })
   } catch (e) {
     ;(e as Error).message = `Command failed: ${command}`
     throw e
