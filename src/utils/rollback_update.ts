1a72accc384340c92a4632f2baae98bd7553890d
Anders Windelhed <anders.windelhed@gmail.com>
2026-07-06 16:38:45 +0200

    Use git reset --hard for rollback so staged changes from a failed commit are also reverted, since git checkout . restores from the index and misses them

--- src/utils/rollback_update.ts
+++ src/utils/rollback_update.ts
@@ -3,7 +3,7 @@ import type PackageManager from '../types/package_manager'
 import installPackages from './install_packages'
 
 const rollbackUpdate = async (packageManager: PackageManager) => {
-  await execa`git checkout .`
+  await execa('git', ['reset', '--hard'])
 
   await installPackages(packageManager)
 }
