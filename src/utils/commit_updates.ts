3f6d3bdb5e4d1af71013d8d391357d439e4fcb23
Anders Windelhed <anders.windelhed@gmail.com>
2026-07-06 21:18:36 +0200

    Simplify commit message building by extracting a helper with a guard clause for the single-update case, removing the redundant firstUpdate check

--- src/utils/commit_updates.ts
+++ src/utils/commit_updates.ts
@@ -3,19 +3,23 @@ import type Update from '../types/update'
 import describeUpdate from './describe_update'
 
 const commitUpdates = async (updates: Update[]) => {
-  const [firstUpdate] = updates
-  const messageArguments =
-    updates.length === 1 && firstUpdate
-      ? ['-m', `Updating package ${describeUpdate(firstUpdate)}`]
-      : [
-          '-m',
-          'Updating packages',
-          '-m',
-          updates.map((update) => `- ${describeUpdate(update)}`).join('\n'),
-        ]
+  const messageArguments = generateMessageArguments(updates)
 
   await execa('git', ['add', '--all'])
   await execa('git', ['commit', ...messageArguments])
+}
+
+const generateMessageArguments = (updates: Update[]) => {
+  if (updates.length === 1) {
+    return ['-m', `Updating package ${describeUpdate(updates[0])}`]
+  }
+
+  return [
+    '-m',
+    'Updating packages',
+    '-m',
+    updates.map((update) => `- ${describeUpdate(update)}`).join('\n'),
+  ]
 }
 
 export default commitUpdates
