261fea1de5a9e7c79f06930aaef66af52d84ea65
Anders Windelhed <anders.windelhed@gmail.com>
2026-07-07 12:43:05 +0200

    Code formatting

--- src/utils/package_version.ts
+++ src/utils/package_version.ts
@@ -7,12 +7,15 @@ const packageVersion = () => {
     const packageJsonPath = findPackageJSON(pathToFileURL(__filename).href)
     if (!packageJsonPath) {
       console.error('Could not find package.json')
+
       process.exit(1)
     }
+
     const { version } = JSON.parse(readFileSync(packageJsonPath, 'utf8'))
     return version
   } catch {
     console.error('Could not read package.json')
+
     process.exit(1)
   }
 }
